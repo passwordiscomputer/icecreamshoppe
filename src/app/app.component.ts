86e9f5eb204f9cafee67570082d201d096bfed2c
roger yee <ryee.epicodus@gmail.com>
2018-03-28 22:26:38 -0700

    formats table, minor wording changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Bucket } from './models/bucket.model';
 export class AppComponent {
   title = 'app';
   masterBucketList: Bucket[] = [
-    new Bucket('chocolate', 2.49, '#6b3e26', "white"),
+    new Bucket('chocolate', 2.49, '#6b3e26', 'white'),
     new Bucket('strawberry', 1.99, 'pink'),
     new Bucket('vanilla', 2.49, 'beige')
   ];
--- src/app/models/bucket.model.ts
+++ src/app/models/bucket.model.ts
@@ -2,23 +2,24 @@
 export class Bucket {
   public remainingScoops = 96;
   public currentScoops = 0;
-  constructor(public flavor: string, public scoopPrice: number, public color: string, public fontColor: string = "black") { }
+  constructor(public flavor: string, public scoopPrice: number, public color: string, public fontColor: string = 'black') { }
 
-  addScoop(){
+  addScoop() {
     if (this.remainingScoops > 0) {
-        this.remainingScoops -= 1;
-        this.currentScoops +=1;
+      this.remainingScoops -= 1;
+      this.currentScoops += 1;
+    } else {
+      alert(`Sorry, We're out of that flavor!\n\nPlease pick another one!` );
     }
   }
 
-  subtractScoop(){
+  subtractScoop() {
     if (this.currentScoops > 0) {
-        this.remainingScoops += 1;
-        this.currentScoops -=1;
+      this.remainingScoops += 1;
+      this.currentScoops -= 1;
     }
   }
 
-  totalPrice(){
-    return( Math.round((this.currentScoops * this.scoopPrice)*100)/100);
+  totalPrice() {
+    return (Math.round((this.currentScoops * this.scoopPrice) * 100) / 100);
   }
-}
